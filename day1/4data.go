79f82640a6c8c46e2b669dd2c5b70cd37241fa81
Bolt Qiao <379763376@163.com>
2019-07-21 23:34:01 +0800

    day1

---
+++ day1/4data.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	//1.枚举 iota 常量自动生成器，每个一行 自动加1
+}
--- user/hello/hello.go
+++ user/hello/hello.go
@@ -3,9 +3,19 @@ package main
 import (
 	"Bolt_golang_way/user/stringutil"
 	"fmt"
+	//"bufio"
+	//"io"
 )
+
+func myf2()  {
+	
+}
 
 func main() {
 	fmt.Printf("hello, world\n")
 	fmt.Printf(stringutil.Reverse("!oG,olleH"))
+	//reader := bufio.Reader{}
+	//fmt.Println(reader)
+	//reader2 := io.Reader([byte(1)])
+	//fmt.Println(reader2)
 }
