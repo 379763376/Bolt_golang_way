24d3e7e7e46fac37a4e6e66fa6244677cc0e3941
Bolt Qiao <379763376@163.com>
2019-08-08 07:50:42 +0800

    ''

--- daydayup7/28指针数组 数组元素为数组指针.go
+++ daydayup7/28指针数组 数组元素为数组指针.go
@@ -11,9 +11,15 @@ func main() {
 	//指针数组  数组元素 是指针类型
 	var arr [3]*int = [3]*int{&a, &b, &c}
 
+
+	//数组指针 存放的是数组的地址  *[3]int   [3]int的指针
+	//指针数组 存放的是一些变量的地址  [3]*int  指针类型的数组
+
+
 	fmt.Println(arr)
 
-	*arr[1] = 200 //先arr[1]是一个地址，*
+	*arr[1] = 200 //先arr[1]值是一个地址，*arr[1]代表地址arr[1]的值 赋值为200
+
  	fmt.Println(b)
 
 	for i := 0; i < len(arr); i++ {
@@ -28,13 +34,14 @@ func main01() {
 
 	//p := &a
 	//fmt.Println(p)
-
+	//数组中的元素是  数组指针    定义了数组[3]类型为数组指针*[3]int
 	var arr [3]*[3]int = [3]*[3]int{&a,&b,&c}
 
 	fmt.Println(arr)
 	fmt.Printf("%T\n",arr)
 
-	(*arr[1])[1] = 555
+	(*arr[1])[1] = 555  //*arr[1] 为
+	arr[1][2] = 666
 	fmt.Println(b)
 
 
